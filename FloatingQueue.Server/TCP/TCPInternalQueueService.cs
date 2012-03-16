f33c88dd41efbd2fa49c2d59368d3571dd800cee
pmosorin <boss@eleks.com>
2012-03-16 17:23:34 +0200

    Added support of new methods for TCPInternalQueueService

--- FloatingQueue.Server/TCP/TCPInternalQueueService.cs
+++ FloatingQueue.Server/TCP/TCPInternalQueueService.cs
@@ -28,9 +28,14 @@ namespace FloatingQueue.Server.TCP
             if (request.Command == "IntroduceNewNode".GetHashCode())
                 return IntroduceNewNode(service, request, response);
             if (request.Command == "RequestSynchronization".GetHashCode())
-                return RequestSynchronization(service, request, response);
-            if (request.Command == "NotificateSlaveSynchronized".GetHashCode())
-                return NotificateSlaveSynchronized(service, request, response);
+                    return RequestSynchronization(service, request, response);
+            if (request.Command == "NotificateNodeIsSynchronized".GetHashCode())
+                    return NotificateNodeIsSynchronized(service, request, response);
+            if (request.Command == "ReceiveAggregateEvents".GetHashCode())
+                    return ReceiveAggregateEvents(service, request, response);
+            if (request.Command == "NotificateAllAggregatesSent".GetHashCode())
+                return NotificateAllAggregatesSent(service, request, response);
+
             return false;
         }
 
@@ -56,37 +61,67 @@ namespace FloatingQueue.Server.TCP
         //void IntroduceNewNode(NodeInfo nodeInfo);
         protected bool IntroduceNewNode(IInternalQueueService service, TCPBinaryReader request, TCPBinaryWriter response)
         {
-            throw new NotImplementedException();
-            //var nodeInfo = ReadNodeInfo(request);
-            //service.IntroduceNewNode(nodeInfo);
-            //
+            var nodeInfo = ReadNodeInfo(request);
+            service.IntroduceNewNode(nodeInfo);
             return true;
         }
 
-        //RequestSynchronization(int serverId, IDictionary<string, int> currentAggregateVersions)
+        //void RequestSynchronization(int serverId, IDictionary<string, int> currentAggregateVersions)
         protected bool RequestSynchronization(IInternalQueueService service, TCPBinaryReader request, TCPBinaryWriter response)
         {
-            throw new NotImplementedException();
-            
-            //var nodeInfo = ReadNodeInfo(request);
-            //service.RequestSynchronization(nodeInfo);
-            //
+            var serverId = request.ReadInt32();
+            var count = request.ReadInt32();
+            var dic = new Dictionary<string, int>(count);
+            for (int i = 0; i < count; i++)
+            {
+                var key = request.ReadString();
+                var value = request.ReadInt32();
+                dic.Add(key, value);
+            }
+            service.RequestSynchronization(serverId, dic);
             return true;
         }
 
 
         //void NotificateNodeIsSynchronized(int serverId)
-        protected bool NotificateSlaveSynchronized(IInternalQueueService service, TCPBinaryReader request, TCPBinaryWriter response)
+        protected bool NotificateNodeIsSynchronized(IInternalQueueService service, TCPBinaryReader request, TCPBinaryWriter response)
         {
-            throw new NotImplementedException();
-            var nodeInfo = ReadNodeInfo(request);
-            //service.NotificateSlaveSynchronized(nodeInfo);
-            //
+            var serverId = request.ReadInt32();
+            service.NotificateNodeIsSynchronized(serverId);
             return true;
         }
 
-
         //void ReceiveAggregateEvents(string aggregateId, int version, IEnumerable<object> events) 
+        protected bool ReceiveAggregateEvents(IInternalQueueService service, TCPBinaryReader request, TCPBinaryWriter response)
+        {
+            var aggId = request.ReadString();
+            var version = request.ReadInt32();
+            var count = request.ReadInt32();
+            var dic = new List<object>(count);
+            for (int i = 0; i < count; i++)
+            {
+                var obj = request.ReadObject();
+                dic.Add(obj);
+            }
+            service.ReceiveAggregateEvents(aggId, version, dic);
+            return true;
+            
+        }
+
+        //void NotificateAllAggregatesSent(IDictionary<string, int> writtenAggregatesVersions);
+        protected bool NotificateAllAggregatesSent(IInternalQueueService service, TCPBinaryReader request, TCPBinaryWriter response)
+        {
+            var count = request.ReadInt32();
+            var dic = new Dictionary<string, int>(count);
+            for (int i = 0; i < count; i++)
+            {
+                var key = request.ReadString();
+                var value = request.ReadInt32();
+                dic.Add(key, value);
+            }
+            service.NotificateAllAggregatesSent(dic);
+            return true;
+        }
 
     }
 }
