8aced27c4300f28900319bba36f4032801e7452d
Markiyan Matsekh <mr.matsekh@gmail.com>
2012-03-23 17:43:48 +0200

    used NDesk.Options native methods for method ShowUsage

--- FloatingQueue.Server/Program.cs
+++ FloatingQueue.Server/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.ServiceModel;
 using FloatingQueue.Common;
+using FloatingQueue.Common.Common;
 using FloatingQueue.Common.TCP;
 using FloatingQueue.Common.TCPProvider;
 using FloatingQueue.Common.WCF;
@@ -22,20 +24,16 @@ namespace FloatingQueue.Server
         private static CommunicationObjectBase ms_InternalHost;
         private static CommunicationObjectBase ms_PublicHost;
         private static List<string> ms_NodesAddresses;
+        private static OptionSet ms_Arguments;
 
         public static void Main(string[] args)
         {
-            if (args == null || args.Length == 0)
-            {
-                ShowUsage();
-                return;
-            }
-
             try
             {
                 if (!Initialize(args))
                 {
-                    Core.Server.Log.Error("Couldn't initialize server.");
+                    // Core.Server.Log may be not initialized yet
+                    Logger.Instance.Error("Couldn't initialize server.");
                     Console.ReadLine();
                     return;
                 }
@@ -55,8 +53,7 @@ namespace FloatingQueue.Server
             }
             catch (Exception ex)
             {
-                // logger may be not initialized here
-                Console.WriteLine("AHTUNG! Unhandled Exception!!!{0}{1}", Environment.NewLine, ex);
+                Logger.Instance.Error("AHTUNG! Unhandled Exception!!!{0}{1}", Environment.NewLine, ex);
                 Console.ReadLine();
             }
         }
@@ -67,17 +64,23 @@ namespace FloatingQueue.Server
         {
             try
             {
-                InitializeCommunicationProvider(useTCP: true);
-
                 var configuration = ParseConfiguration(args);
+
+                InitializeCommunicationProvider(useTCP: true);
 
                 var componentsManager = new ComponentsManager();
                 var container = componentsManager.GetContainer(configuration);
                 Core.Server.Init(container);
             }
+            catch (BadConfigurationException ex)
+            {
+                Logger.Instance.Warn("Bad configuration: {0}",ex.Message);
+                ShowUsage();
+                return false;
+            }
             catch (Exception ex)
             {
-                Core.Server.Log.Warn("Error while initializing server{0}{1}{0}{2}", Environment.NewLine, ex.Message, ex.StackTrace);
+                Logger.Instance.Warn("Error while initializing server{0}{1}{0}{2}", Environment.NewLine, ex.Message, ex.StackTrace);
                 return false;
             }
             return true;
@@ -154,47 +157,43 @@ namespace FloatingQueue.Server
 
         #region Helper Methods
 
-        private static void InitializeCommunicationProvider(bool useTCP)
-        {
-            if (useTCP)
-            {
-                // TCP
-                var provider = new TCPCommunicationProvider();
-                provider.RegisterHostImplementation<PublicQueueService>(() => new TCPPublicQueueService());
-                provider.RegisterHostImplementation<InternalQueueService>(() => new TCPInternalQueueService());
-                provider.RegisterChannelImplementation<IInternalQueueService>(() => new TCPInternalQueueServiceProxy());
-                CommunicationProvider.Init(provider);
-            }
-            else
-            {
-                // WCF
-                var provider = new WCFCommunicationProvider();
-                CommunicationProvider.Init(provider);
-            }
-        }
-
         private static ServerConfiguration ParseConfiguration(string[] args)
         {
+            if (args == null || args.Length ==0)
+                throw new BadConfigurationException("arguments are empty");
+
             const string addressMask = "{0}:{1}";
             const string localAddress = "net.tcp://localhost";
 
-            var configuration = new ServerConfiguration { ServerId = 0};
+            var configuration = new ServerConfiguration { ServerId = 0 };
             int publicPort = 80, internalPort = 81;
             byte serverId = 255;
             bool isMaster = false,
                  idSet = false;
             ms_NodesAddresses = new List<string>();
 
-            var p = new OptionSet()
+            ms_Arguments = new OptionSet
                     {
-                        {"pp|pubport=", v => int.TryParse(v, out publicPort)},
-                        {"ip|intport=", v => int.TryParse(v, out internalPort)},
-                        {"m|master", v => isMaster = !string.IsNullOrEmpty(v) },
-                        {"id=",v => idSet = byte.TryParse(v, out serverId) },
-                        {"n|nodes=", v => ms_NodesAddresses.AddRange( v.Split(';')) }
+                        {"pp|pubport=", "int16 {PORT} for listening public requests",
+                            v => int.TryParse(v, out publicPort)},
+                        {"ip|intport=", "int16 {PORT} for listening internal requests",
+                            v => int.TryParse(v, out internalPort)},
+                        {"m|master", "mark current server as master ",
+                            v => isMaster = !string.IsNullOrEmpty(v) },
+                        {"id=", "unique int16 {ID} of this server",
+                            v => idSet = byte.TryParse(v, out serverId) },
+                        {"n|nodes=", "a list of {NODES} for initial connect to cluster",
+                            v => ms_NodesAddresses.AddRange( v.Split(';')) }
                     };
-            p.Parse(args);
-
+            try
+            {
+                ms_Arguments.Parse(args);
+            }
+            catch (OptionException ex)
+            {
+                throw new BadConfigurationException(ex.Message);
+            }
+            
             if (ms_NodesAddresses.Count == 0 && !isMaster)
                 throw new BadConfigurationException("At least 1 node should be set at startup");
 
@@ -219,6 +218,25 @@ namespace FloatingQueue.Server
             return configuration;
         }
 
+        private static void InitializeCommunicationProvider(bool useTCP)
+        {
+            if (useTCP)
+            {
+                // TCP
+                var provider = new TCPCommunicationProvider();
+                provider.RegisterHostImplementation<PublicQueueService>(() => new TCPPublicQueueService());
+                provider.RegisterHostImplementation<InternalQueueService>(() => new TCPInternalQueueService());
+                provider.RegisterChannelImplementation<IInternalQueueService>(() => new TCPInternalQueueServiceProxy());
+                CommunicationProvider.Init(provider);
+            }
+            else
+            {
+                // WCF
+                var provider = new WCFCommunicationProvider();
+                CommunicationProvider.Init(provider);
+            }
+        }
+
         private static CommunicationObjectBase CreateHost<T>(string address)
         {
             var host = CommunicationProvider.Instance.CreateHost<T>(typeof(T).Name, address);
@@ -232,11 +250,21 @@ namespace FloatingQueue.Server
 
         private static void ShowUsage()
         {
-            // todo : use NDesk.Options.WriteOptionDescriptions method here
-            Console.WriteLine("Usage: {0} <arg1> .. <argN>", AppDomain.CurrentDomain.FriendlyName);
-            Console.WriteLine("Arguments:");
-            Console.WriteLine("\tp|port=(int16) - port to run server");
-            Console.WriteLine("\tm|master - mark server as master");
+            Logger.Instance.Warn("Usage:");
+            Logger.Instance.Warn("{0} <arg1> .. <argN>", AppDomain.CurrentDomain.FriendlyName);
+            Logger.Instance.Warn("Arguments:");
+            if (ms_Arguments != null)
+            {
+                using (var writer = new StringWriter())
+                {
+                    ms_Arguments.WriteOptionDescriptions(writer);
+                    Logger.Instance.Warn("{0}{1}",Environment.NewLine, writer.ToString());
+                }
+            }
+            else
+            {
+                Logger.Instance.Warn("Oops! Error occured before parsing arguments");
+            }
         }
 
         #endregion
